0f28d8f1d4a02c8a00c570bb6741478498cc353c
Ryouko <153080407+Ryouko-Yamanda65777@users.noreply.github.com>
2024-11-12 15:28:36 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ from rvc import Config, load_hubert, get_vc, rvc_infer
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-mdxnet_models_dir = os.path.join(BASE_DIR, 'mdxnet_models')
+mdxnet_models_dir = os.path.join(BASE_DIR, 'src')
 rvc_models_dir = os.path.join(BASE_DIR, 'rvc_models')
 output_dir = os.path.join(BASE_DIR, 'song_output')
 
